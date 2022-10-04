8f0f4dfbb15688e03da1e6528dc223ff564e3f0f
jdrichards-pursuit <jdrpursuit@gmail.com>
2022-10-04 12:08:45 -0400

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+// component use import... from
+// helper functions import...from
+// css just use import
+
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Main Component</h1>
+    </div>
+  );
+};
+
+export default Main;
